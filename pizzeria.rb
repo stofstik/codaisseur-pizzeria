0374e825076f0986042f86b6226fbb26c17ee3ce
stofstik <verstegen.daan@gmail.com>
2016-10-05 10:08:54 +0200

    angry waiter

--- pizzeria.rb
+++ pizzeria.rb
@@ -3,3 +3,4 @@ require './waiter.rb'
 w = Waiter.new
 
 w.greet_guest
+w.serve_guest
--- waiter.rb
+++ waiter.rb
@@ -4,13 +4,20 @@ class Waiter
     end
 
     def take_order(order_num)
-
+        case order_num
+            when 1
+                p "Let me get the menu"
+            when 2
+                p "Goodbye!"
+            else
+                p "Que catso fai?"
+        end
     end
 
     def serve_guest
         p "How can I help you?"
         p "1. Would you like to order?"
         p "2. Would you like to leave?"
-        take_order(get.chomp.to_i)
+        take_order(gets.chomp.to_i)
     end
 end
